bde8ef572d0f15ba66a4b974b3efc3457b595d12
Manish Jadhav <54273848+ManishJadhav100@users.noreply.github.com>
2022-04-18 09:50:30 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -8,11 +8,11 @@ class Config:
     BOT_TOKEN = "5392802848:AAEKmK2Qfxdg5v3t46DzgQGhkT5JvMgfOeI"
     SESSION_NAME = os.environ.get('SESSION_NAME')
     LOG_CHANNEL = -1001771465683
-    DATABASE_URL = "postgres://mshanzhzfzpwmy:f68e91231beab13e16ef7529a9ab01301211cc2420e60e45441f83eeba68fcc0@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d8hu8n0dmkcmfu"
+    DATABASE_URL = "mongodb+srv://mjfreeflix:Manya004@cluster0.9oay3.mongodb.net/screenshots?retryWrites=true&w=majority"
     AUTH_USERS = [int(i) for i in os.environ.get('1464063686').split(' ')]
     MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 2))
     MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 120))
-    TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
+    TRACK_CHANNEL = -1001771465683
     SLOW_SPEED_DELAY = int(os.environ.get('SLOW_SPEED_DELAY', 15))
     HOST = os.environ.get('HOST', '')
     
